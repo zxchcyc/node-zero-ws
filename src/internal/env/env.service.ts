fc86b34b97f4f55340aaf85d6355d92b8c62d499
潇畅 <>
2022-04-03 10:57:42 +0800

    build: env

--- src/internal/env/env.service.ts
+++ src/internal/env/env.service.ts
@@ -10,16 +10,12 @@ export class EnvService {
   private envConfig: EnvConfig;
 
   constructor(@Inject(ENV_OPTIONS) options: EnvOptions) {
-    // if (process.env.NODE_ENV === 'dev') {
     // 配置文件路径
     const filePath = `.env.${
       process.env.NODE_ENV ? process.env.NODE_ENV : 'dev'
     }`;
     const envFile = resolve(__dirname, '../../', options.folder, filePath);
     this.envConfig = parse(readFileSync(envFile));
-    // } else {
-    // this.envConfig = process.env;
-    // }
   }
 
   // 热加载
